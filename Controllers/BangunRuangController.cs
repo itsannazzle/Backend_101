8a41eb8589677bc379c72324c3970b79a80fb249
tsi-annakarenina <karenina.os@brins.co.id>
2022-10-18 15:49:21 +0700

    -encode decode every service -add base controller

--- Controllers/BangunRuangController.cs
+++ Controllers/BangunRuangController.cs
@@ -4,10 +4,11 @@ using System.Text;
 using System.Text.Json;
 using backend_101.Constants;
 using backend_101.Function;
+using backend_101.Controllers;
 
 [ApiController]
 
-public class BangunRuangController : ControllerBase
+public class BangunRuangController : BaseController
 {
     [HttpPost]
     [Route("[controller]/calculatePersegi")]
@@ -22,36 +23,51 @@ public class BangunRuangController : ControllerBase
 
         if(stringToken != null)
         {
+
             RequestModel modelRequestInternal = new RequestModel();
             modelUser.Token = stringToken;
 
-            modelRequestInternal.Data = JsonSerializer.Serialize<UserModel>(modelUser);
+            string stringData = base64Decode(modelRequest.Data);
 
-             modelResponse = UtilityFunction.callInternalService(WebAddressConstant.STRING_SCHME_HTTP + WebAddressConstant.STRING_SCHME_LOCALHOST + WebAddressConstant.STRING_PORT_KIDZ +  WebAddressConstant.STRING_KIDZ_VALIDATEUSERTOKEN,modelRequestInternal);
-
-            if (modelResponse.ServiceResponseCode == ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_SUCCESS)
+            if(!(string.IsNullOrEmpty(stringData)))
             {
-                JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+                string stringModelRequestDataSerialize = JsonSerializer.Serialize<UserModel>(modelUser);
+                modelRequestInternal.Data = base64Encode(stringModelRequestDataSerialize);
+
+                modelResponse = UtilityFunction.callInternalService(WebAddressConstant.STRING_SCHME_HTTP + WebAddressConstant.STRING_SCHME_LOCALHOST + WebAddressConstant.STRING_PORT_KIDZ +  WebAddressConstant.STRING_KIDZ_VALIDATEUSERTOKEN,modelRequestInternal);
+
+                if (modelResponse.ServiceResponseCode == ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_SUCCESS)
                 {
-                    PropertyNameCaseInsensitive = true,
-                };
+                    JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    };
 
-                modelUser = JsonSerializer.Deserialize<UserModel>(modelRequest.Data, serializerOptions);
-                modelPersegi = modelUser.modelPersegi;
-                modelPersegi.Result = modelPersegi.countLuasBangunan();
-                modelUser.modelPersegi = modelPersegi;
-                Console.Write(modelPersegi.detailBangunan());
+                    modelUser = JsonSerializer.Deserialize<UserModel>(modelResponse.Data, serializerOptions);
+                    modelPersegi = JsonSerializer.Deserialize<PersegiModel>(stringData, serializerOptions);;
+                    modelPersegi.Result = modelPersegi.countLuasBangunan();
+                    modelUser.modelPersegi = modelPersegi;
+                    Console.Write(modelPersegi.detailBangunan());
 
-                modelResponse.ServiceResponseCode = ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_SUCCESS;
-                modelResponse.MessageTitle = StringConstant.STRING_MESSAGE_TITLE_SUCCESS;
-                modelResponse.Data = JsonSerializer.Serialize<UserModel>(modelUser, serializerOptions);
+                    modelResponse.ServiceResponseCode = ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_SUCCESS;
+                    modelResponse.MessageTitle = StringConstant.STRING_MESSAGE_TITLE_SUCCESS;
+                    string modelUserSerialize = JsonSerializer.Serialize<UserModel>(modelUser, serializerOptions);
+                    modelResponse.Data = base64Encode(modelUserSerialize);
+
+                }
+                else
+                {
+                    modelResponse.ServiceResponseCode = ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_FAIL;
+                    modelResponse.MessageContent = StringConstant.STRING_MESSAGE_CONTENT_TOKEN_NOTFOUND;
+                    modelResponse.MessageTitle = StringConstant.STRING_MESSAGE_CONTENT_ACCESS_DENY;
+                }
 
             }
             else
             {
-                modelResponse.ServiceResponseCode = ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_FAIL;
-                modelResponse.MessageContent = StringConstant.STRING_MESSAGE_CONTENT_TOKEN_NOTFOUND;
-                modelResponse.MessageTitle = StringConstant.STRING_MESSAGE_CONTENT_ACCESS_DENY;
+                modelResponse.ServiceResponseCode = ServiceResponseCodeConstant.STRING_RESPONSECODE_MODULE_SUCCESS;
+                modelResponse.MessageContent = StringConstant.STRING_MESSAGE_CONTENT_JSONSERIALIZE_FAIL;
+                modelResponse.MessageTitle = StringConstant.STRING_MESSAGE_TITLE_FAIL;
             }
 
         }
